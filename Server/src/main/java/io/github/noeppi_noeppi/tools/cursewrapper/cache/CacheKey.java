e00bcda60a07a988b70a0b99ed74aad6caad0e2e
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-01-25 19:11:06 +0100

    Make file caches larger as that is definitely required with some mods

--- Server/src/main/java/io/github/noeppi_noeppi/tools/cursewrapper/cache/CacheKey.java
+++ Server/src/main/java/io/github/noeppi_noeppi/tools/cursewrapper/cache/CacheKey.java
@@ -11,8 +11,8 @@ public class CacheKey<K, V> {
     
     public static final CacheKey<Integer, String> SLUG = new CacheKey<>(400, 1000 * 60 * 60 * 5);
     public static final CacheKey<Integer, ProjectInfo> PROJECT = new CacheKey<>(100, 1000 * 60 * 60 * 3);
-    public static final CacheKey<FileKey, FileInfo> FILE = new CacheKey<>(200, 1000 * 60 * 60 * 3);
-    public static final CacheKey<FileKey, String> CHANGELOG = new CacheKey<>(200, 1000 * 60 * 60);
+    public static final CacheKey<FileKey, FileInfo> FILE = new CacheKey<>(1000, 1000 * 60 * 60 * 3);
+    public static final CacheKey<FileKey, String> CHANGELOG = new CacheKey<>(300, 1000 * 60 * 60);
     public static final CacheKey<SearchKey, List<Integer>> SEARCH = new CacheKey<>(200, 1000 * 60 * 30);
     public static final CacheKey<FilesKey, List<FileKey>> FILES = new CacheKey<>(200, 1000 * 60 * 30);
     
