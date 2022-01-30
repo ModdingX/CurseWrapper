306622b94e3c46f32bc12215f9dba0a216013e97
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-01-30 14:14:07 +0100

    Some docker stuff that hopefully works

---
+++ Server/src/docker/start.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os.path
+import json
+import subprocess
+
+config = {}
+if os.path.isfile('config.json'):
+    with open('config.json', mode='r') as file:
+        config = json.loads(file.read())
+
+args = ['java', '-p', 'server.jar', '-p', 'dependencies']
+args += ['-m', 'io.github.noeppi_noeppi.tools.cursewrapper.server']
+
+args += ['--docker']
+
+if 'no-ssl' in config and bool(config['no-ssl']):
+    args += ['--no-ssl']
+
+if 'port' in config:
+    args += ['--port', config['port']]
+
+if 'threads' in config:
+    args += ['--threads', config['threads']]
+
+subprocess.call(args)
---
+++ docker-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "CurseWrapper Docker for version ${1}"
+
+ZIP_FILE=$(mktemp)
+BUILD_DIR=$(mktemp -d)
+
+echo "Downloading Image"
+wget "-O" "${ZIP_FILE}" "https://noeppi-noeppi.github.io/MinecraftUtilities/maven/io/github/noeppi_noeppi/tools/CurseWrapperServer/${1}/CurseWrapperServer-${1}-docker.zip"
+
+echo "Extracting Image"
+unzip "${ZIP_FILE}" "-d" "${BUILD_DIR}"
+
+if [[ -f "config.json" ]]; then
+  cp "config.json" "${BUILD_DIR}/config.json"
+fi
+
+echo "Building Image cursewrapper_${1//-/_}"
+cd "${BUILD_DIR}" || exit
+docker "build" "-t" "cursewrapper_${1//-/_}" "."
+
+if docker "service" "inspect" "cursewrapper" 2> /dev/null > /dev/null; then
+  echo "Updating Image in Docker Service"
+  docker "service" "update" "--image" "cursewrapper_${1//-/_}" "cursewrapper"
+else
+  echo "Creating Docker Service"
+  docker "service" "create" "--name" "cursewrapper" "--secret" "curse_token" "--secret" "ssl_keystore" "--secret" "ssl_keystore_password" "cursewrapper_${1//-/_}" 
+fi
